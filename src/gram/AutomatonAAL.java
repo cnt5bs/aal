6780641a321c9c75b4a39039b01b8242ffd297a2
italo <italosvg@gmail.com>
2016-12-17 00:36:36 -0200

    + ‘src/gram/AutomatonAAL.java’ + ‘src/gram/AutomatonBuilder.java’ + ‘src/gram/Main.java’ + ‘src/gram/antlr4/AASL.tokens’ + ‘src/gram/antlr4/AASLBaseListener.java’ + ‘src/gram/antlr4/AASLLexer.java’ + ‘src/gram/antlr4/AASLLexer.tokens’ + ‘src/gram/antlr4/AASLListener.java’ + ‘src/gram/antlr4/AASLParser.java’ + ‘test/aa.aal’ + ‘test/af.aal’ + ‘test/ape.aal’

---
+++ src/gram/AutomatonAAL.java
@@ -0,0 +1,20 @@
+
+package gram;
+import br.usp.poli.lta.cereda.aa.execution.AdaptiveAutomaton;
+import br.usp.poli.lta.cereda.aa.model.Submachine;
+import br.usp.poli.lta.cereda.aa.model.Transition;
+public class AutomatonAAL extends AdaptiveAutomaton {
+
+    public void add(Transition t) {
+        transitions.add(t);
+    }
+
+    @Override
+    public void setup() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    void addSubMachine(Submachine nova) {
+        submachines.add(nova);
+    }
+}
